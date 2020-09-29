bf5c4bd84fc174980fb1aead7e6f5c377254b29f
MasaNakura <46548890+MasaNakura@users.noreply.github.com>
2020-09-29 12:21:08 -0700

    Update UsingStacksSuitorsLab.java

--- UsingStacksSuitorsLab.java
+++ UsingStacksSuitorsLab.java
@@ -14,49 +14,49 @@ import java.util.*;
  */
 
 public class UsingStacksSuitorsLab implements Runnable {
-	private static int threadCount = 0;
-	private String name;
+   private static int threadCount = 0;
+   private String name;
 	
-	public UsingStacksSuitorsLab() {
-		name = "#" + threadCount++ + "Thread";
-	}
+   public UsingStacksSuitorsLab() {
+      name = "#" + threadCount++ + "Thread";
+   }
 	
-	public static void main(String[] args) {
-		String s1 = "food";		    //not a palindrome
-		String s2 = "racecar";      //a palindrome
-			
-		System.out.println("String1 is \"" + s1 + "\"");
-		System.out.println("String2 is \"" + s2 + "\"");
-		
-	   /*System.out.println(s1 + " reversed is: ");
-		printReverse(s1);
-		System.out.println(s2 + " reversed is: ");
-		printReverse(s2);
+   public static void main(String[] args) {
+      String s1 = "food";		    //not a palindrome
+      String s2 = "racecar";      //a palindrome
+   		
+      System.out.println("String1 is \"" + s1 + "\"");
+      System.out.println("String2 is \"" + s2 + "\"");
+   	
+      /*System.out.println(s1 + " reversed is: ");
+   	printReverse(s1);
+   	System.out.println(s2 + " reversed is: ");
+   	printReverse(s2);
       
-	   recPrintReverse(s1);
-		System.out.println();
-		recPrintReverse(s2);
-		System.out.println();
-		System.out.println(s1 + " is a palindrome: " + isPalindrome(s1));
-		System.out.println(s2 + " is a palindrome: " + isPalindrome(s2));
+      recPrintReverse(s1);
+   	System.out.println();
+   	recPrintReverse(s2);
+   	System.out.println();
+   	System.out.println(s1 + " is a palindrome: " + isPalindrome(s1));
+   	System.out.println(s2 + " is a palindrome: " + isPalindrome(s2));
       
-		System.out.println(s1 + " is a palindrome(recursively): " + isPalindromeRec(s1));
-		System.out.println(s2 + " is a palindrome(recursively): " + isPalindromeRec(s2));	
-		
-		System.out.println("Did we build a Queue of Threads and start them? " + buildThreadQueue());
-		
-		int n = 6;
-		System.out.println("For " + n + " suitors, stand in place:" + findPlaceToStand(n));
-		
-		n = 10;
-		System.out.println("For " + n + " suitors, stand in place:" + findPlaceToStand(n));*/
-	}
+   	System.out.println(s1 + " is a palindrome(recursively): " + isPalindromeRec(s1));
+   	System.out.println(s2 + " is a palindrome(recursively): " + isPalindromeRec(s2));	
+   	*/
+   	System.out.println("Did we build a Queue of Threads and start them? " + buildThreadQueue());
+   	
+   	int n = 6;
+   	System.out.println("For " + n + " suitors, stand in place:" + findPlaceToStand(n));
+   	
+   	n = 10;
+   	System.out.println("For " + n + " suitors, stand in place:" + findPlaceToStand(n));
+   }
 		
 	
 	
-	
-	public static void printReverse(String target) {
-		//todo: use a stack
+	// prints the given String in a reverse order using iteration
+   public static void printReverse(String target) {
+   	//todo: use a stack
       Stack<Character> reverse = new Stack<>();
       int size = target.length();
       for (int i = 0; i < size; i++) {
@@ -66,21 +66,22 @@ public class UsingStacksSuitorsLab implements Runnable {
          System.out.print(reverse.pop());
       }
       System.out.println();
-      
-	}
+   }
 	
-	public static void recPrintReverse(String target) {
-		//todo
+   // prints the given String in a reverse order using recursion
+   public static void recPrintReverse(String target) {
+   	//todo
       if (target.length() == 1) {
          System.out.print(target);
       } else if (target.length() > 1){
          System.out.print(target.charAt(target.length() - 1));
          recPrintReverse(target.substring(0, target.length() - 1));
       }
-	}
+   }
 	
-	public static boolean isPalindrome(String input) {
-		//todo: use a stack
+   // returns true if the given String is a palindrome and false if otherwise using iteration
+   public static boolean isPalindrome(String input) {
+   	//todo: use a stack
       Stack<Character> reverse = new Stack<>();
       int size = input.length();
       String reversed = "";
@@ -92,10 +93,11 @@ public class UsingStacksSuitorsLab implements Runnable {
       }
       return input.equals(reversed);
       
-	}
+   }
 
-	public static boolean isPalindromeRec(String sentence)	{
-	  	//todo
+   // returns true if the given String is a palindrome and false if otherwise by using recursion
+   public static boolean isPalindromeRec(String sentence)	{
+     	//todo
       if (sentence.length() <= 1) {
          return true;
       } else {
@@ -106,57 +108,93 @@ public class UsingStacksSuitorsLab implements Runnable {
          }
          return false;        
       }   
-	}
+   }
 	
-	public static int findPlaceToStand(int numSuitors) {
-		Queue<Integer> queue = new LinkedList<>();
-        for(int i = 0; i < numSuitors; i++) {
-            queue.add(i+1);
-        }
-        int modulus = 3;
-        int i = 1;
-        while(queue.size() > 1) { 
-            int candidate = queue.poll();
-            if(i != modulus) {
-                queue.add(candidate);
-            }
+   // finds what number from 1 to the given number that will be chosen if every 3 counted off
+   // is removed
+   public static int findPlaceToStand(int numSuitors) {
+      Queue<Integer> queue = new LinkedList<>();
+      for(int i = 0; i < numSuitors; i++) {
+         queue.add(i+1);
+      }
+      int modulus = 3;
+      int i = 1;
+      while(queue.size() > 1) { 
+         int candidate = queue.poll();
+         if(i != modulus) {
+            queue.add(candidate);
+         }
       
-            i = (i % mod) + 1; 
-        }
-        return queue.poll();
-	}	
+         i = (i % modulus) + 1; 
+      }
+      return queue.poll();
+   }	
+   /*
+   // finds what number from 1 to the given number that will be chosen if every 3 counted off
+   // is removed. When the counting reaches the maximum number, it recounts the current max
+   // and removes every 3 counted off downwards. 
+   public static int findPlaceToStand(int numSuitors) {
+      Stack<Integer> s1 = new Stack<>();
+      Stack<Integer> s2 = new Stack<>();
+      for (int i = numSuitors; i >= 1; i--) {
+         s1.push(i);
+      }
+      int i = 1;
+      int count = numSuitors;
+      Stack<Integer> other = s2;
+      Stack<Integer> using = s1;
+      while (count > 1) {
+         if (i == 3) {
+            using.pop();
+            count--;
+            i = 1;
+         } else {
+            other.push(using.pop());
+            i++;
+         }
+         if (using.isEmpty()) {
+            Stack<Integer> temp = using;
+            using = other;
+            other = temp;         
+         }
+      }
+      if (using.isEmpty()) {
+         return other.peek();
+      } else {
+         return using.peek();
+      }
+   }
+   */
 
-
-	public static boolean buildThreadQueue() {	//returns true upon success
-		Queue<Thread> q = new LinkedList<Thread>(); 
-		
-		//when our program starts up, it might create multiple threads to use
-		q.add(new Thread(new UsingStacksSuitorsLab()));
-		q.add(new Thread(new UsingStacksSuitorsLab()));
-		q.add(new Thread(new UsingStacksSuitorsLab()));
-		
-		System.out.println("Initial Thread order:");
-		q.toString();  
-		
-		//We need to iterate over our pool of threads and call start() on each one
-		//Make a loop that dequeues a thread, calls start on it, and //enqueues it again
-		//to do:
-		//current = get a thread
-		//current.start();
-		//put the thread back
-
-		for (int i = 0; i < q.size(); i++) {
-			current = q.peek();
-			q.poll();
-			current.start();
-			q.add(current);
-		}
-		
-		System.out.println("Thread order after start()ing:");
-		q.toString();  
-		
-		return true;  //on successful start
-	}
+   public static boolean buildThreadQueue() {	//returns true upon success
+      Queue<Thread> q = new LinkedList<Thread>(); 
+   	
+   	//when our program starts up, it might create multiple threads to use
+      q.add(new Thread(new UsingStacksSuitorsLab()));
+      q.add(new Thread(new UsingStacksSuitorsLab()));
+      q.add(new Thread(new UsingStacksSuitorsLab()));
+   	
+      System.out.println("Initial Thread order:");
+      q.toString();  
+   	
+   	//We need to iterate over our pool of threads and call start() on each one
+   	//Make a loop that dequeues a thread, calls start on it, and //enqueues it again
+   	//to do:
+   	//current = get a thread
+   	//current.start();
+   	//put the thread back
+   
+      for (int i = 0; i < q.size(); i++) {
+         Thread current = q.poll();
+         current.start();
+         q.add(current);
+      }
+   	
+      System.out.println("Thread order after start()ing:");
+      q.toString();  
+   	
+      return true;  //on successful start
+   }
 	
 	
 	/*
@@ -165,15 +203,15 @@ public class UsingStacksSuitorsLab implements Runnable {
 	 * behaviour of each thread in the thread pool above
 	 */
 	
-	@Override
-	public void run() {
-		for(int i = 0; i < 1000; i++) {
-			System.out.println(name + ": " + i + "th iteration");
-			try {
-				Thread.sleep(10);
-			} catch (InterruptedException e) {
-				//do nothing here
-			}
-		}
-	}
+   @Override
+   public void run() {
+      for(int i = 0; i < 1; i++) {
+         System.out.println(name + ": " + i + "th iteration");
+         try {
+            Thread.sleep(10);
+         } catch (InterruptedException e) {
+         	//do nothing here
+         }
+      }
+   }
 }
