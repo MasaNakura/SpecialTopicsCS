83fb44ae1b84890d37bc408136110e8afcf096c0
Your Name <aaronxie2003@gmail.com>
2020-11-09 15:38:13 -0800

    updated bubble sort

--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -4,18 +4,22 @@ public class SortingAlgorithms {
 
     }
 
-    public static void bubbleSort(int array[]) {
-        int n = array.length;
-        int k;
-        for (int m = n; m >= 0; m--) {
-            for (int i = 0; i < n - 1; i++) {
-                k = i + 1;
-                if (array[i] > array[k]) {
-                    swapNumbers(i, k, array);
-                }
-            }
-            printNumbers(array);
-        }
+    public static int[] bubbleSort(int arr[]) {
+        int n = arr.length;  
+        int temp = 0;  
+         for(int i=0; i < n; i++){  
+                 for(int j=1; j < (n-i); j++){  
+                          if(arr[j-1] > arr[j]){  
+                                 //swap elements  
+                                 temp = arr[j-1];  
+                                 arr[j-1] = arr[j];  
+                                 arr[j] = temp;  
+                         }  
+                          
+                 }  
+         } 
+
+         retirn arr;
     }
 
     public static int[] insertionSort(int[] input){
