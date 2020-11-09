3b42baa269262b88ce5dde894d85a3fbd62346a9
Your Name <aaronxie2003@gmail.com>
2020-11-09 13:50:34 -0800

    added bubble and insertion sorting algorithms

--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -4,4 +4,33 @@ public class SortingAlgorithms {
 
     }
 
+    public static void bubbleSort(int array[]) {
+        int n = array.length;
+        int k;
+        for (int m = n; m >= 0; m--) {
+            for (int i = 0; i < n - 1; i++) {
+                k = i + 1;
+                if (array[i] > array[k]) {
+                    swapNumbers(i, k, array);
+                }
+            }
+            printNumbers(array);
+        }
+    }
+
+    public static int[] insertionSort(int[] input){
+         
+        int temp;
+        for (int i = 1; i < input.length; i++) {
+            for(int j = i ; j > 0 ; j--){
+                if(input[j] < input[j-1]){
+                    temp = input[j];
+                    input[j] = input[j-1];
+                    input[j-1] = temp;
+                }
+            }
+        }
+        return input;
+    }
+
 }
